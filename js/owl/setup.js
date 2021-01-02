d31f88ec41e82d54b8ec283589baac17d537bea7
Daniel Araujo <danifatec201530@gmail.com>
2021-01-02 13:24:58 -0300

    Finalizando Clone Netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 5,
+    responsiveClass: true,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 3,
+        },
+        1000: {
+            items: 5,
+        }
+    }
+})
